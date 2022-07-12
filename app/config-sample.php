adbc2acd22615c73f427c83d77f78594a1416055
Leandro Martins Guimarães <leandropl@gmail.com>
2022-07-11 21:53:37 -0300

    Git CI/CD config params

--- app/config-sample.php
+++ app/config-sample.php
@@ -2,3 +2,9 @@
 // Bot name
 $TELEGRAM_CHANNEL_ID = "channel-id";
 $TELEGRAM_BOT_TOKEN = "bot-token";
+
+// CI/CD
+$private_dir = '/private/dir/';
+$public_dir = '/public/dir/';
+$github_repo_name = '';
+$github_secret = '';
