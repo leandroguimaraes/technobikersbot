e38e9485e386542a28b7570d7e1dd7778faa3286
Leandro Martins Guimarães <leandropl@gmail.com>
2022-07-12 00:21:45 -0300

    Code refactoring and save send msg info

--- app/index.php
+++ app/index.php
@@ -1,53 +1,107 @@
 <?php
-require_once("config.php");
-
-global $TELEGRAM_CHANNEL_ID, $TELEGRAM_BOT_TOKEN;
-
 date_default_timezone_set('America/Sao_Paulo');
 
-$months = [
-  'JAN',
-  'FEV',
-  'MAR',
-  'ABR',
-  'MAI',
-  'JUN',
-  'JUL',
-  'AGO',
-  'SET',
-  'OUT',
-  'NOV',
-  'DEZ'
-];
+require_once("config.php");
 
-$daysToSunday = 7 - date('w');
-$today = date('Y-m-d');
-$sundayTime = strtotime("{$today} + {$daysToSunday} days");
+global $TELEGRAM_BOT_TOKEN;
 
-$sunday = date('d/', $sundayTime).$months[date('n', $sundayTime)-1];
+$dbFile = 'db.php';
+$url = "https://api.telegram.org/bot{$TELEGRAM_BOT_TOKEN}/";
 
-$url = "https://api.telegram.org/bot{$TELEGRAM_BOT_TOKEN}/sendPoll";
+$isTodayFriday = date('w') == 5;
+$isTimeToSend = date('H') == 9;
+if ($isTodayFriday && $isTimeToSend) {
+  $sendMsg = false;
+  $db = getDbData();
+  if (!isset($db->lastSent) || date('Y-m-d', strtotime($db->lastSent)) != date('Y-m-d')) {
+    $sendMsg = true;
+  }
 
-$data = array(
-  "chat_id" => "-{$TELEGRAM_CHANNEL_ID}",
-  "question" => "Você vai ao pedal deste domingo {$sunday}?",
-  "options" => ["Bora! 🚴‍♂️", "Neste, não... 👎"],
-  "type" => "quiz",
-  "correct_option_id" => 0,
-  "is_anonymous" => "False"
-);
+  if ($sendMsg) {
+    sendMsg();
+  }
+}
 
-$additional_headers = array(
-  'Content-Type: application/json'
-);
+function sendMsg() {
+  createPoll();
+}
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers);
+function createPoll() {
+  $months = [
+    'JAN',
+    'FEV',
+    'MAR',
+    'ABR',
+    'MAI',
+    'JUN',
+    'JUL',
+    'AGO',
+    'SET',
+    'OUT',
+    'NOV',
+    'DEZ'
+  ];
+  
+  $daysToSunday = 7 - date('w');
+  $today = date('Y-m-d');
+  $sundayTime = strtotime("{$today} + {$daysToSunday} days");
+  
+  $sunday = date('d/', $sundayTime).$months[date('n', $sundayTime)-1];
 
-$server_output = curl_exec($ch);
+  $data = array(
+    "question" => "Você vai ao pedal deste domingo {$sunday}?",
+    "options" => ["Bora! 🚴‍♂️", "Neste, não... 👎"],
+    "type" => "quiz",
+    "correct_option_id" => 0,
+    "is_anonymous" => "False"
+  );
 
-header('Content-Type: application/json');
-echo json_encode(json_decode($server_output), JSON_PRETTY_PRINT);
+  $db = getDbData();
+  $db->lastSent = date('Y-m-d H:i');
+  saveDbData($db);
+
+  $result = doPost($data, 'sendPoll');
+  $db->messageId = $result->result->message_id;
+
+  saveDbData($db);
+}
+
+function getDbData() {
+  global $dbFile;
+
+  $dbStr = @file_get_contents($dbFile);
+  if ($dbStr) {
+    return json_decode($dbStr);
+  } else {
+    return new stdClass();
+  }
+}
+
+function saveDbData($data) {
+  global $dbFile;
+
+  file_put_contents($dbFile, json_encode($data));
+}
+
+function doPost($data, $endPoint) {
+  global $TELEGRAM_CHANNEL_ID, $url;
+
+  $data['chat_id'] = "-{$TELEGRAM_CHANNEL_ID}";
+
+  $additional_headers = array(
+    'Content-Type: application/json'
+  );
+  
+  $ch = curl_init($url.$endPoint);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers);
+
+  return json_decode(curl_exec($ch));
+}
+
+function echoResult($result) {
+  header('Content-Type: application/json');
+  echo json_encode($result, JSON_PRETTY_PRINT);
+}
